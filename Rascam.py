ac63eb9ee71d88267030b6a8c83f3f77d4cb67f5
Raspi <edhim911@gmail.com>
2024-08-08 15:05:14 +1000

    update Rascam.py

--- Rascam.py
+++ Rascam.py
@@ -3,7 +3,7 @@ from time import sleep
 from libcamera import Transform
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL, transform=Transform(hflip=True))
+picam2.start_preview(Preview.QTGL, transform=Transform(vflip=True))
 picam2.start()
 picam2.capture_file("/home/team9/Desktop/image2.jpg")
 sleep(5)
