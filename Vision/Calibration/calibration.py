d1df5c1a1306d2cd53bccc36b4541dfd3c96f119
ahhim911 <edhim911@gmail.com>
2024-09-20 14:36:43 +1000

    update calibrate order

--- Vision/Calibration/calibration.py
+++ Vision/Calibration/calibration.py
@@ -21,15 +21,15 @@ class Calibration:
 
     
     def calibrate(self, focal_length=True, homography=True, color=True):
-        if focal_length:
-            cal_focal_length = CalibrateFocalLength()
-            cal_focal_length.calibrate()
-        if homography:
-            cal_holomography = CalibrateHomography()
-            cal_holomography.calibrate()
         if color:
             cal_color = CalibrateColor()
             cal_color.calibrate()
+        if homography:
+            cal_holomography = CalibrateHomography()
+            cal_holomography.calibrate()
+        if focal_length:
+            cal_focal_length = CalibrateFocalLength()
+            cal_focal_length.calibrate()
         return 
     
 def main():
