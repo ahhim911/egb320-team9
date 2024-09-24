881a444f139ecc538a0cb73b039555f05ae5dfce
Edmond Chan <edhim911@gmail.com>
2024-09-24 16:50:02 +1000

    Detect in Image

--- Vision/Detection/test_detection.py
+++ Vision/Detection/test_detection.py
@@ -42,22 +42,30 @@ def detect_in_image(image):
     shelf_detector = Shelf()
     #item_detector = Item()
     marker_detector = Marker()
+    wall_detector = Wall()
+    #ramp_detector = PackingStationRamp()
     #obstacle_detector = Obstacle()
 
-    # Detect objects in the image
-    #detected_shelves, shelf_frame, mask = shelf_detector.find_shelf(image, color_ranges)
-    #detected_items, item_frame = item_detector.find_item(image, color_ranges)
-    detected_markers, marker_frame, mask = marker_detector.find_marker(image, color_ranges)
-    #detected_obstacles, obstacle_frame = obstacle_detector.find_obstacle(image, color_ranges)
+    # Detect objects in the current frame
+    detected_shelves, shelf_frame, shelf_mask = shelf_detector.find_shelf(frame, color_ranges)
+    #detected_items, item_frame = item_detector.find_item(frame, color_ranges)
+    detected_walls, filled_wall_mask, wall_mask = wall_detector.find_wall(frame, color_ranges)
+    detected_markers, marker_frame, marker_mask = marker_detector.find_marker(frame, filled_wall_mask, color_ranges)
+    #detected_obstacles, obstacle_frame = obstacle_detector.find_obstacle(frame, color_ranges)
+    #detected_ramps, wall_frame, wall_mask = wall_detector.find_wall(frame, color_ranges)
 
     # Display the detection results
-    #cv2.imshow('Shelf Detection', shelf_frame)
-    cv2.imshow('Mask Detection', mask)
+    cv2.imshow('Shelf Detection', shelf_frame)
+    cv2.imshow('Shelf Mask', shelf_mask)
     #cv2.imshow('Item Detection', item_frame)
+    
     cv2.imshow('Marker Detection', marker_frame)
+    cv2.imshow('Marker Mask', marker_mask)
+    #cv2.imshow('Wall Detection', filled_wall_mask)
+    cv2.imshow('Wall Mask', filled_wall_mask)
     #cv2.imshow('Obstacle Detection', obstacle_frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 def detect_in_video(video_path):
     """Detect objects in each frame of a video and display live preview."""
